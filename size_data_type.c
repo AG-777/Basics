d6bbc0abf1bc9cce09f2cde7a6a73baa2ff1e398
AG-777 <apuravgautam721@gmail.com>
2024-01-16 02:35:23 +0530

    added files

---
+++ size_data_type.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+  int a = 16;
+  printf("Size of variable a: %zu\n",sizeof(a));
+  printf("Size of char data type: %zu\n",sizeof(char));
+  printf("Size of int data type: %zu\n",sizeof(int));
+  printf("Size of float data type: %zu\n",sizeof(float));
+  printf("Size of double data type: %",sizeof(double));
+}
