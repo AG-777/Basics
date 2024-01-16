3536d7e88d744b79609f07bf3d3abbd0f48bb21c
AG-777 <apuravgautam721@gmail.com>
2024-01-16 23:08:44 +0530

    modified grid

--- programs/grid.c
+++ programs/grid.c
@@ -3,24 +3,30 @@
 int get_size(void);
 void print_grid(int size);
 
-int main(void){
-  //get size of grid
+int main(void)
+{
+  // get size of grid
   int n = get_size();
-  //print grid
+  // print grid
   print_grid(n);
 }
 
-int get_size(void){
+int get_size(void)
+{
   int n;
-  scanf("%d",&n);
+  do
+  {
+    scanf("%d", &n);
+  } while (n < 1);
+
   return n;
 }
 
 void print_grid(int size)
 {
-  for ( int i = 0; i < size ; i++)
+  for (int i = 0; i < size; i++)
   {
-    for ( int j = 0; j < size; j++)
+    for (int j = 0; j < size; j++)
     {
       printf("#");
     }
