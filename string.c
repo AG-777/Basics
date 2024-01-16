f09b6c86f8800e5d9e71c4e50c3167362733cca5
AG-777 <apuravgautam721@gmail.com>
2024-01-16 20:47:40 +0530

    How to use string in C

---
+++ string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    // Declare a character array to store the string
+    char myString[5];
+
+    // Ask the user to enter a string
+    printf("Enter a string: ");
+
+    // Use scanf to get the string from the user
+    scanf("%4s", myString);
+
+    // Display the entered string
+    printf("You entered: %s\n", myString);
+
+    return 0;
+}
