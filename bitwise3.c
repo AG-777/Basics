496c1e73426628d8de4f06c17d9c7a41e6979334
AG-777 <apuravgautam721@gmail.com>
2024-01-19 00:33:16 +0530

    bitwise one's complement

--- bitwise3.c
+++ bitwise3.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main()
 {
-  int n1 = 9,n2;
-  n2 = ~n1;
-  printf("~n1: %d\n",n2);
+  unsigned int n1 = 15,n2;
+  n2 = ~n1; 
+  int n3 = 15,n4;
+  n4 = ~n3;
+  printf("~n1(signed): %u\n",n2);
+  printf("~n3(unsigned): %d\n",n4);
 }
